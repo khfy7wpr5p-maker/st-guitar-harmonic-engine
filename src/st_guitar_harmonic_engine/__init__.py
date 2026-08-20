a837ba57672e312e95c571535159150d14a68f6c
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-08-20 13:39:28 +0300

    Stage 0-C: add deterministic polyphonic frames

--- src/st_guitar_harmonic_engine/__init__.py
+++ src/st_guitar_harmonic_engine/__init__.py
@@ -1,5 +1,14 @@
 """ST Guitar Harmonic Engine deterministic core."""
 
+from .frames import HarmonicFrame, build_harmonic_frames
 from .models import Measure, NoteEvent, RationalBeat, TieState, TimeSignature
 
-__all__ = ["Measure", "NoteEvent", "RationalBeat", "TieState", "TimeSignature"]
+__all__ = [
+    "HarmonicFrame",
+    "Measure",
+    "NoteEvent",
+    "RationalBeat",
+    "TieState",
+    "TimeSignature",
+    "build_harmonic_frames",
+]
